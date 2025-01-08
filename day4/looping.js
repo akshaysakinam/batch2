d25480846290c69d4be719779837067658a7bf92
sakinamakshay@gmail.com <sakinamakshay@gmail.com>
2025-01-08 13:17:16 +0530

    day4

---
+++ day4/looping.js
@@ -0,0 +1,56 @@
+// console.log("hello");
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("javascript");
+// }
+
+
+//printing up 100
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+//odd and even numbers
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`${i} is even`);
+//     } else {
+//         console.log(`${i} is odd`);
+//     }
+// }
+
+
+
+//factorial
+
+// let number = parseInt(prompt("Enter number:"))
+// let ans = 1;
+// if (number < 0) {
+//     console.log("Invalid Input");
+// }
+// else if (number === 1) {
+//     console.log(1);
+// }
+// else {
+//     for (let i = 1; i <= number; i++) {
+//         ans *= i;
+//     }
+//     console.log(ans);
+// }
+
+
+//while
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log("hello");
+//     i++;
+// }
+
+//do-while
+let j = 1;
+do {
+    console.log("hello");
+    j++;
+} while (j >= 5)
