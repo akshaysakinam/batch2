bc86fc66c6444c183d4a44c7e2da78a0f404aea1
Akshay Kumar Sakinam <sakinamakshay@gmail.com>
2025-01-30 11:35:18 +0530

    day17

---
+++ 17_day17/file.js
@@ -0,0 +1,9 @@
+// export let msg ='hello Morning'
+// export function greet(){
+//     return msg +"Ima coding"
+// }
+export let msg ='hello Morning'
+ function greet(){
+    return msg +"Ima coding"
+}
+export default greet;
---
+++ 17_day17/file2.js
@@ -0,0 +1,5 @@
+import greet, { msg } from "./file.js";
+
+let message=greet()
+console.log(message)
+console.log(msg)
