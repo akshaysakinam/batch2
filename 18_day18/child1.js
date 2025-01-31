4d0320197636e45afca972c481369efd6d3da071
Akshay Kumar Sakinam <sakinamakshay@gmail.com>
2025-01-31 11:32:03 +0530

    day 18

---
+++ 18_day18/child1.js
@@ -0,0 +1,36 @@
+// let x=10;
+// export default x;
+
+// let userDetails={
+//     name:"Adarsh",
+//     profession:"Student"
+// }
+// export default userDetails;
+
+
+// !named export
+
+// let x =10;
+// const user="Sanjay";
+// const college="MRU"
+// const users=["AKshay","Adarsh","Harish"];
+// const obj={
+//     company:"ExcelR"
+// }
+
+// export { 
+//     x,user,users,college,obj
+// }
+
+
+export let x =10;
+export const user="Sanjay";
+
+export const users=["AKshay","Adarsh","Harish"];
+export const obj={
+    company:"ExcelR"
+}
+
+let college="MRU";
+export default college;
+
---
+++ 18_day18/file.js
@@ -0,0 +1,22 @@
+// import x from "./child1.js";
+// console.log(x);
+
+
+//named import
+
+import college,{x,user,users, obj} from "./child1.js";
+console.log(college);
+console.log(x);
+
+console.log(user);
+users.map((user)=>{
+    console.log(user)
+})
+
+console.log(obj.company);
+
+
+
+
+
+
